204f657ee250b01ade1aa550158865db8f6ccb51
Suyash Lale <suyashale90@gmail.com>
2024-01-08 00:13:38 +0530

    Added frontend folder and initial structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { CreateTodo } from "./components/CreateTodo";
+
+function App() {
+  return (
+    <div>
+      <CreateTodo />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,34 @@
+export function CreateTodo() {
+  return (
+    <div>
+      <input
+        style={{
+          padding: 10,
+          margin: 10,
+        }}
+        type="text"
+        placeholder="Title"
+      />
+      <br />
+      <br />
+      <input
+        style={{
+          padding: 10,
+          margin: 10,
+        }}
+        type="text"
+        placeholder="Description"
+      />
+      <br />
+      <br />
+      <button
+        style={{
+          padding: 10,
+          margin: 10,
+        }}
+      >
+        Add a Todo
+      </button>
+    </div>
+  );
+}
