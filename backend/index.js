798378ab836686ace4a276173f8a2a24a5b4bbcd
Suyash Lale <suyashale90@gmail.com>
2024-01-07 23:29:54 +0530

    Added route implementations

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,31 @@
 const express = require("express");
-const { createTodo, updateTodo } = requrie("../types.js");
+const { createTodo, updateTodo } = require("../types.js");
+const { todo } = require("./db");
 
 const app = express();
 app.use(express.json());
 
 // See all todo items
-app.get("/todos", (req, res) => {});
+app.get("/todos", async (req, res) => {
+  try {
+    const todoArr = await todo.find({});
+    res.status(200).json({
+      Todos: todoArr,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      msg: "Cannot get Todos from the DB",
+    });
+  }
+});
 
 // Create a new todo item
 // {
 //     title: String,
 //     description: String
 // }
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if (!parsedPayload.success) {
@@ -22,10 +35,25 @@ app.post("/todo", (req, res) => {
     return;
   }
   // put in Mongo DB here
+  try {
+    await todo.create({
+      title: createPayload.title,
+      description: createPayload.description,
+      completed: false,
+    });
+    res.status(200).json({
+      msg: "Todo created",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      msg: "Failed to create Todo in DB",
+    });
+  }
 });
 
 // Mark a todo item done
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const payload = req.body;
   const parsedPayload = updateTodo.safeParse(payload);
   if (!parsedPayload.success) {
@@ -35,6 +63,24 @@ app.put("/completed", (req, res) => {
     return;
   }
   // Update item in Mongo DB here
+  try {
+    await todo.updateOne(
+      {
+        _id: payload.id,
+      },
+      {
+        completed: true,
+      }
+    );
+    res.status(200).json({
+      msg: "Todo marked complete",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      msg: "Failed to update Todo in the DB",
+    });
+  }
 });
 
 app.listen(3000, () => console.log("Backend running on port 3000"));
